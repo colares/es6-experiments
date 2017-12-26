22b0ffa0e57cf6476b1796a97fb5e3adc2bdf699
Thiago Colares <thicolares@gmail.com>
2017-12-26 20:54:29 -0300

    New methods for strings

---
+++ 13.new-methods-for-string.js
@@ -0,0 +1,7 @@
+let text = "Lorem ipsum dolor sit amet"
+console.log(text.startsWith('Lorem'))
+console.log(text.startsWith('rem', 2))
+console.log(text.endsWith('ame'))
+console.log(text.endsWith('ame', 25))
+console.log('lol'.repeat(5))
+console.log(text.includes('dolor'))
