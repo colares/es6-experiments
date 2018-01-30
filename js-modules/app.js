adcf9f0adf03d965a4dd307e2a73393626090444
Thiago Colares <thicolares@gmail.com>
2018-01-29 21:47:10 -0300

    Use alias for one metho when importing

--- js-modules/app.js
+++ js-modules/app.js
@@ -4,6 +4,7 @@ const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6]
 const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1]
 
 const arr3 = junta(arr1, arr2)
-const arr3 = uniq(arr1, arr2)
+const arr4 = uniq(arr1, arr2)
 
 console.log(arr3)
+console.log(arr4)
