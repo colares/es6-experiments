103bfff4379c3d83f24dbbe1f67857059dda4b07
Thiago Colares <thicolares@gmail.com>
2018-02-10 22:27:29 -0300

    Enable source-map

--- js-modules/webpack.config.js
+++ js-modules/webpack.config.js
@@ -4,6 +4,7 @@ const nodeENV = process.env.NODE_ENV || 'production';
 // toda configuração do webpack
 // sem virá aqui
 module.exports = {
+    devtool: 'source-map',
     entry: {
         filename: './app.js'
     },
@@ -33,7 +34,7 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin({
             compress: {warnings: false},
             output: {comments: false, beautify: false},
-
+            sourceMap: true
         }),
         new webpack.DefinePlugin({
             'process.env': { NODE_ENV: JSON.stringify(nodeENV)}
